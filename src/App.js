b41463e3d9dd9f4bdfaec42bac251d20c42593d2
LuizFelipe <luizfelipers19@gmail.com>
2021-01-09 14:18:18 -0300

    files commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Monte a sua Pizza!</h1>
+      <h3>A combinação dos nossos ingredientes e a sua fome!</h3>
+      <p>Use a sua criatividade!</p>
     </div>
   );
 }
